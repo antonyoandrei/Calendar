e064ee537e56e49402f2b62ab803eaa7623d3d0c
Ivan Martin Lomoro <ivanmartin_95@hotmail.com>
2023-09-06 10:24:45 +0200

    Added functionality active-day

--- dist/main.js
+++ dist/main.js
@@ -23,6 +23,38 @@ const months = [
     "November",
     "December"
 ];
+const eventsArr = [
+    {
+        day: 16,
+        month: 9,
+        year: 2023,
+        events: [
+            {
+                title: "Event 1 lorem ipsun dolar sit genfa tersd dsad ",
+                time: "10:00 AM",
+            },
+            {
+                title: "Event 2",
+                time: "11:00 AM",
+            },
+        ],
+    },
+    {
+        day: 18,
+        month: 9,
+        year: 2023,
+        events: [
+            {
+                title: "Event 1 lorem ipsun dolar sit genfa tersd dsad ",
+                time: "10:00 AM",
+            },
+            {
+                title: "Event 2",
+                time: "11:00 AM",
+            },
+        ],
+    },
+];
 function initCalendar() {
     const firstDay = new Date(year, month, 0);
     const lastDay = new Date(year, month + 1, 0);
@@ -37,19 +69,38 @@ function initCalendar() {
         days += `<div class="day prev-date">${prevDays - x + 1}</div>`;
     }
     for (let i = 1; i <= lastDate; i++) {
+        let event = false;
+        eventsArr.forEach((eventObj) => {
+            if (eventObj.day === i &&
+                eventObj.month === month + 1 &&
+                eventObj.year === year) {
+                event = true;
+            }
+        });
         if (i === new Date().getDate() &&
             year === new Date().getFullYear() &&
             month === new Date().getMonth()) {
-            days += `<div class="day today active">${i}</div>`;
+            if (event) {
+                days += `<div class="day today active event">${i}</div>`;
+            }
+            else {
+                days += `<div class="day today active">${i}</div>`;
+            }
         }
         else {
-            days += `<div class="day">${i}</div>`;
+            if (event) {
+                days += `<div class="day event">${i}</div>`;
+            }
+            else {
+                days += `<div class="day">${i}</div>`;
+            }
         }
     }
     for (let j = 1; j <= nextDays; j++) {
         days += `<div class="day next-date">${j}</div>`;
     }
     daysContainer.innerHTML = days;
+    addListner();
 }
 initCalendar();
 function prevMonth() {
@@ -79,6 +130,9 @@ todayBtn.addEventListener("click", () => {
 const addEventBtn = document.querySelector(".add-event");
 const addEventContainer = document.querySelector(".add-event-wrapper");
 const addEventCloseBtn = document.querySelector(".close");
+const addEventTitle = document.querySelector(".event-name");
+const addEventFrom = document.querySelector(".event-time-from");
+const addEventTo = document.querySelector(".event-time-to");
 addEventBtn.addEventListener("click", () => {
     addEventContainer === null || addEventContainer === void 0 ? void 0 : addEventContainer.classList.toggle("active");
 });
@@ -93,3 +147,45 @@ document.addEventListener("click", (e) => {
         addEventContainer.classList.remove("active");
     }
 });
+addEventTitle.addEventListener("input", (e) => {
+    addEventTitle.value = addEventTitle.value.slice(0, 50);
+});
+function addListner() {
+    const days = document.querySelectorAll(".day");
+    days.forEach((day) => {
+        day.addEventListener("click", (e) => {
+            const target = e.target;
+            activeDay = Number(target.innerHTML);
+            days.forEach((day) => {
+                day.classList.remove("active");
+            });
+            if (target.classList.contains("prev-date")) {
+                prevMonth();
+                setTimeout(() => {
+                    const days = document.querySelectorAll(".day");
+                    days.forEach((day) => {
+                        if (!day.classList.contains("prev-date") &&
+                            day.innerHTML === target.innerHTML) {
+                            day.classList.add("active");
+                        }
+                    });
+                }, 100);
+            }
+            else if (target.classList.contains("next-date")) {
+                nextMonth();
+                setTimeout(() => {
+                    const days = document.querySelectorAll(".day");
+                    days.forEach((day) => {
+                        if (!day.classList.contains("next-date") &&
+                            day.innerHTML === target.innerHTML) {
+                            day.classList.add("active");
+                        }
+                    });
+                }, 100);
+            }
+            else {
+                target.classList.add("active");
+            }
+        });
+    });
+}
